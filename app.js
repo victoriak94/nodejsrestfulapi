1f175d101a3e86292660d953766c67520cc8b082
victoriak94 <vkamila6@gmail.com>
2019-05-11 13:40:52 -0400

    Create first middleware

--- app.js
+++ app.js
@@ -6,10 +6,19 @@ const app = express();
 
 //NOW WE HAVE THE ABILITY TO CREATE ROUTES!!
 
+//Middlewares
+app.use('/posts', () => {
+    console.log('This is a middleware running')
+})
 //Routes
 app.get("/", (req, res) => {
   res.send("We are on home");
 });
+//there is also app.post, app.delete, app.patch for other CRUD actions
+
+app.get"/posts", (req, res) => {
+    res.send("We are on posts");
+  });
 
 //How do we start listening to the server?
 app.listen(3000); //port name that we want to listen to
