d377f850161c5f2aa42dd2ee610441feca20a838
victoriak94 <vkamila6@gmail.com>
2019-05-11 13:33:40 -0400

    Create first route home

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ const express = require("express");
 const app = express();
 
 //NOW WE HAVE THE ABILITY TO CREATE ROUTES!!
+
+//Routes
+app.get('/' (req,res) => {
+    res.send('We are on home');
+});
+
+//How do we start listening to the server?
+app.listen(3000); //port name that we want to listen to
